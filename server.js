b3c5d3fc020b0d6a19370fc671ab400c354ff3d7
ThuyPhuocThinh <thuyphuocthinhtpt1@gmail.com>
2024-11-19 20:00:04 +0700

    init project

---
+++ server.js
@@ -0,0 +1,13 @@
+const app = require("./src/app");
+const PORT = 5000;
+
+const server = app.listen(PORT, () => {
+  console.log(`Backend starts with port ${PORT}`);
+});
+
+// SIGINT = Ctrl + C
+process.on("SIGINT", () => {
+  server.close(() => {
+    console.log("Exit Server Express");
+  });
+});
---
+++ src/app.js
@@ -0,0 +1,19 @@
+// 106k + 10k
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const compression = require("compression");
+const app = express();
+
+// init middleware
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+
+// init db
+
+// init router
+
+// handle error
+
+module.exports = app;
