d581c247013636ef5bb9454f16a7fe9eaa60b400
ThuyPhuocThinh <thuyphuocthinhtpt1@gmail.com>
2024-11-20 15:35:17 +0700

    config env

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require("./src/app");
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
   console.log(`Backend starts with port ${PORT}`);
--- src/app.js
+++ src/app.js
@@ -3,8 +3,7 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const compression = require("compression");
-const { checkOverload } = require("./helpers/check.connect.js");
-
+require("dotenv").config();
 
 const app = express();
 
@@ -15,7 +14,6 @@ app.use(compression());
 
 // init db
 require("./database/init.mongodb.level1.js");
-// checkOverload();
 
 // init router
 
